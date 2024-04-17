ff7b8121561fca2d69ec125b334647bd4bbcf6c2
giova <giovani.baccelliere2.0@gmail.com>
2024-04-16 23:15:55 -0400

    avance pagina

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # proyect-split5
 proyecto split 5
+
+## creacion de aplicacion web 
+en esta app se crearon conjuntos de tablas en los cuales organizaban datos de vehiculos en venta segun el modelo y los precios
+dejando asi segun lo que se busque al consumidor 
--- app.py
+++ app.py
@@ -2,10 +2,38 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('vehicles_us.csv')
-hist_button = st.button('vehiculos')
 
-if hist_button:
-    st.write('conjunto de datos de anuncios de venta de coches')
-    fig = px.histogram(car_data, x="odometer")
+car_data = pd.read_csv('vehicles_us.csv')
+
+st.header('venta de vehículos')
+build_histogram = st.checkbox('Construir un histograma')
+if build_histogram:
+    st.write('datos variados de coches')
+    fig = px.histogram(car_data, x="cylinders")
     st.plotly_chart(fig, use_container_width=True)
+    
+disp_button = st.button('modelos')
+fig = px.scatter(car_data, x="model", y="model_year")
+fig.show()
+
+data_df = pd.DataFrame(
+    {
+        "model": car_data["model"],
+        "price": car_data["price"],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "price": st.column_config.NumberColumn(
+            "Price (in USD)",
+            help="The price of the product in USD",
+            min_value=3000,
+            max_value=40000,
+            step=1,
+            format="$%d",
+        )
+    },
+    hide_index=True,
+)
