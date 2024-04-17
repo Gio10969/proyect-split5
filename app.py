d27b5daf2f23b68ae78716b39f51d490e692eb2f
giova <giovani.baccelliere2.0@gmail.com>
2024-04-16 23:59:09 -0400

    resultado avanzado

--- app.py
+++ app.py
@@ -5,21 +5,24 @@ import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv')
 
-st.header('venta de vehículos')
-build_histogram = st.checkbox('Construir un histograma')
+st.header(' venta de vehículos')
+build_histogram = st.checkbox('Según cilindros')
 if build_histogram:
     st.write('datos variados de coches')
     fig = px.histogram(car_data, x="cylinders")
     st.plotly_chart(fig, use_container_width=True)
     
-disp_button = st.button('modelos')
-fig = px.scatter(car_data, x="model", y="model_year")
-fig.show()
+st.header('grafica de modelos')
+fig = px.bar(car_data, x='model_year', y= "price" , color = 'fuel' )
 
+st.plotly_chart(fig)
+
+st.header("lista de modelos con años y precios")
 data_df = pd.DataFrame(
     {
         "model": car_data["model"],
         "price": car_data["price"],
+        "year": car_data["model_year"],
     }
 )
 
